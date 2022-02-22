935e6dc670bc9a124387f5d2c924057ea98894a4
Robert Moss <rmoss92@gmail.com>
2022-02-22 08:55:18 -0800

    Added install script for parallelization

---
+++ scripts/install.jl
@@ -0,0 +1,13 @@
+using Pkg
+Pkg.add("Distributed")
+Pkg.add("POMDPs")
+Pkg.add("POMCPOW")
+Pkg.add("Plots")
+Pkg.add("Parameters")
+Pkg.add("Statistics")
+Pkg.add("Random")
+Pkg.add("Distributions")
+Pkg.add("MineralExploration")
+Pkg.add("MixedFidelityModelSelection")
+Pkg.add("ProgressMeter")
+Pkg.add("BSON")
