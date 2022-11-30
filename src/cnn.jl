11522021e1996e917912f4623461d538724a45e6
Robert Moss <rmoss92@gmail.com>
2022-11-30 10:41:13 -0800

    Initial AlphaZero CNN implementation

--- src/cnn.jl
+++ src/cnn.jl
@@ -15,23 +15,61 @@ end
 
 Random.seed!(0) # determinism
 
+USE_ONLY_MEAN_STD = false
+NORMALIZE_TO_SIGN = true
+
 # CNN model
 function LeNet5(; imgsize=(30, 30, 5), out_dim=1)
     out_conv_size = (imgsize[1] ÷ 4 - 3, imgsize[2] ÷ 4 - 3, 16)
 
-    return Chain(
+    m = Chain(
         Conv((5, 5), imgsize[end]=>6, relu),
         MaxPool((2, 2)),
         Conv((5, 5), 6=>16, relu),
-        MaxPool((2, 2)),
+        # MaxPool((2, 2)),
+        MeanPool((2, 2)),
         Flux.flatten,
+        # Dropout(0.5),
         Dense(prod(out_conv_size), 120, relu),
+        Dropout(0.5),
         Dense(120, 84, relu),
-        Dense(84, out_dim)
+        Dropout(0.5),
+        Dense(84, out_dim),
+        # Dropout(0.5)
     )
+
+    if NORMALIZE_TO_SIGN
+        m = Chain(m.layers..., Dense(out_dim, out_dim, tanh))
+    end
+
+    return m
 end
 
-# model = Chain(
+
+function AlphaZero(; imgsize=(30, 30, 5), out_dim=1, kernel_size=(3,3), num_filters=64, bnorm_momentum=0.6f0)
+    out_conv_size = (imgsize[1] ÷ 4 - 3, imgsize[2] ÷ 4 - 3, 16)
+    pad = kernel_size .÷ 2
+
+    m = Chain(
+        # Common    
+        Conv(kernel_size, imgsize[end]=>num_filters, stride=1, pad=pad),
+        BatchNorm(num_filters, relu, momentum=bnorm_momentum),
+
+        # Value head
+        Conv((1, 1), num_filters=>out_dim),
+        BatchNorm(out_dim, relu, momentum=bnorm_momentum),
+        Flux.flatten,
+        Dense(imgsize[1]*imgsize[2]*out_dim, out_dim, relu),
+    )
+
+    if NORMALIZE_TO_SIGN
+        m = Chain(m.layers..., Dense(out_dim, out_dim, tanh))
+    end
+
+    return m
+end
+
+    # model = Chain(
 #     # 28x28 => 14x14
 #     Conv((5, 5), 1=>8, pad=2, stride=2, relu),
 #     # 14x14 => 7x7
@@ -54,11 +92,10 @@ end
 
 # Load the data
 tt_split = 0.8 # train/test split (80/20)
-data = BSON.load(joinpath(@__DIR__, "..", "..", "POMDP-value-estimation", "betazero_training_betazero.bson"))[:data]
+data = BSON.load(joinpath(@__DIR__, "..", "..", "POMDP-value-estimation", "betazero_training_betazero_pomcpow.bson"))[:data]
 x_data, y_data = data[:X], data[:Y]
 
 # Remove channels 3,4 (skewness and kurtosis)
-USE_ONLY_MEAN_STD = false
 if USE_ONLY_MEAN_STD
     x_data = x_data[:, :, [1,2,5], :]
     imgsize = (30,30,3)
@@ -66,13 +103,17 @@ else
     imgsize = (30,30,5)
 end
 
-n_data = size(x_data,4)
+if NORMALIZE_TO_SIGN
+    y_data = sign.(y_data)
+end
+
+n_data = 50_000 # size(x_data,4)
 n_train = Int(n_data ÷ (1/tt_split))
 n_valid = n_data - n_train
 
 perm = randperm(n_data)
 perm_train = perm[1:n_train]
-perm_valid = perm[n_train+1:end]
+perm_valid = perm[n_train+1:n_data]
 
 x_train = x_data[:,:,:,perm_train]
 y_train = y_data[:, perm_train]
@@ -81,7 +122,8 @@ x_valid = x_data[:,:,:,perm_valid]
 y_valid = y_data[:, perm_valid]
 
 # Put model/data onto GPU device
-model = gpu(LeNet5(; imgsize))
+# model = gpu(LeNet5(; imgsize))
+model = gpu(AlphaZero(; imgsize))
 x_train = gpu(x_train)
 y_train = gpu(y_train)
 x_valid = gpu(x_valid)
@@ -93,16 +135,23 @@ train_data = DataLoader((x_train, y_train), batchsize=1024, shuffle=true)
 sqnorm(x) = sum(abs2, x)
 penalty(λ=0) = λ*sum(sqnorm, Flux.params(model))
 error(ŷ, y) = Flux.Losses.mse(ŷ, y)
-loss(x, y) = Flux.Losses.mse(model(x), y) + penalty()
+accuracy(x, y) = mean(sign.(model(x)) .== sign.(y))
+if NORMALIZE_TO_SIGN
+    # loss(x, y) = mean(sign.(model(x)) .!= y)
+    loss(x, y) = Flux.Losses.mse(model(x), y) + penalty()
+else
+    loss(x, y) = Flux.Losses.mse(model(x), y) + penalty()
+end
 
-if false
-# @time begin
-    lr = 0.001 # learning rate
+# if false
+@time begin
+    lr = 0.01 # learning rate
     opt = ADAM(lr)
     θ = Flux.params(model)
     update_frequency = 1
+    plot_frequency = 10
 
-    number_epochs = 1000
+    number_epochs = 1_000
 
     losses_train = []
     losses_valid = []
@@ -114,11 +163,15 @@ if false
         end
         loss_train = loss(x_train, y_train)
         loss_valid = loss(x_valid, y_valid)
+        acc_train = accuracy(x_train, y_train)
+        acc_valid = accuracy(x_valid, y_valid)
         push!(losses_train, loss_train)
         push!(losses_valid, loss_valid)
         if e % update_frequency == 0
-            println("Epoch: ", e, " Loss Train: ", loss_train, " Loss Val: ", loss_valid)
-            plot(xlims=(1, number_epochs), ylims=(0, 6000))
+            println("Epoch: ", e, " Loss Train: ", loss_train, " Loss Val: ", loss_valid, " | Acc. Train: ", acc_train, " Acc. Val: ", acc_valid)
+        end
+        if e % plot_frequency == 0
+            plot(xlims=(1, number_epochs), ylims=(0, NORMALIZE_TO_SIGN ? 1 : 2000))
             plot!(1:e, losses_train, label="training")
             plot!(1:e, losses_valid, label="validation")
             display(plot!())
